55be31c443901331592d476e4d84b3f8257c973d
agustinmfran <agustinmfran@gmail.com>
2023-03-08 17:56:22 +0100

    pulido

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,22 +2,20 @@
 import React from "react";
 import Link from "next/link";
 
-export default function Header() {
+function Header() {
   return (
-    <header className="w-full mx-auto  px-4 sm:px-20 fixed top-0 z-50 shadow bg-white ">
-      <div className="justify-between md:items-center md:flex">
-        <div>
-          <div className="flex items-center justify-between py-3 md:py-5 md:block">
-            <Link href="/" className="cursor-pointer">
-              <div className="container flex items-center space-x-2">
-                <h2 className="text-2xl font-bold ">
-                  MOVIE<span className="text-red-600">finder</span>
-                </h2>
-              </div>
-            </Link>
+    <header className="w-full mx-auto px-4 sm:px-20 fixed top-0 z-50 bg-white ">
+      <div className="flex items-center justify-between py-3 md:py-5 md:block">
+        <Link href="/" className="cursor-pointer">
+          <div className="container flex items-center space-x-2">
+            <h2 className="text-2xl font-bold ">
+              MOVIE<span className="text-red-600">finder</span>
+            </h2>
           </div>
-        </div>
+        </Link>
       </div>
     </header>
   );
 }
+
+export default Header;
